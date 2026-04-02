aa9231f4ea2a706e98422f2e270860dc3340a9d1
Mamun123123123 <almalm90459@gmail.com>
2026-04-02 20:12:47 +0600

    footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import "./app.css"
-import { Link, Outlet } from 'react-router'
+import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './pages/Footer'
+
 const App = () => {
   return (
     <>
-  <Navbar />
-  <main className='min-h-screen'>
-    <Outlet />
-  </main>
-    <footer>footer</footer>
+      <Navbar />
+      <main className='min-h-screen'>
+        <Outlet />
+      </main>
+      <Footer />
     </>
-    
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Shop = () => {
   return (
-    <div>
-      shop page
-    </div>
+    <>
+    
+    </>
   )
 }
 
