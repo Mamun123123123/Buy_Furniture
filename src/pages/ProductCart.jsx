f02cc3aa8a5ffc0fa1617ab35d8a7ebdfb68e02f
Mamun123123123 <almalm90459@gmail.com>
2026-04-03 21:23:16 +0600

    Design Product cart

--- src/pages/ProductCart.jsx
+++ src/pages/ProductCart.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import getImgURL from '../utils/getImageURL'
 
-const ProductCart = ({key,item}) => {
+const ProductCart = ({ item }) => {
   return (
-    <div>
-      ProductsCart
+    <div className="bg-slate-200 rounded-4xl">
+    
+      <div className="rounded-4xl m-2 flex items-center justify-center">
+        <img
+          src={getImgURL(item.imageUrl)}
+          alt={item.name}
+          className="w-20 h-30 object-cover group-hover:scale-110 transition duration-300"
+        />
+      </div>
+      <div className="p-4 space-y-2">
+        
+        <span className="text-xs bg-gray-100 text-gray-600 px-3 py-1 rounded-full">
+          {item.category}
+        </span>
+        <h2 className="text-lg font-bold text-gray-800">
+          {item.name}
+        </h2>
+        <div className="flex items-center text-yellow-400 text-sm">
+          {"⭐".repeat(item.rating)}
+          <span className="text-gray-500 ml-2 text-xs">
+            ({item.rating}.0)
+          </span>
+        </div>
+        <div className="flex items-center justify-between pt-2">
+          <p className="text-lg font-semibold text-green-600">
+            ${item.price}
+          </p>
+
+          <button className="bg-blue-900 text-white px-2 rounded-full hover:bg-blue-600 transition">
+            +
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import {products} from '../utils/Products'
+import { products } from '../utils/Products'
 import ProductCart from './ProductCart'
-const Products = ({heading}) => {
-    const category = ["Chair","Beds","Sofa","Lamp"]
-  return (
-    <>
-     <div>
-        <div>
-            <h2 className='text-4xl font-bold text-center my-8'>{heading}</h2>
-            <div className='bg-slate-400 max-w-md mx-auto sm:rounded-full md:p-2.5 mb-16'>
-                    <div className='flex md:justify-evenly sm:flex-row justify-center'>
-                {category.map((item)=>
-                   <button key={item} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-amber-800 hover:text-white transition-colors`}>{item}</button>
-                )}
-                    </div>
-            </div>
+const Products = ({ heading }) => {
+    const category = ["Chair", "Beds", "Sofa", "Lamp"]
+    return (
+        <>
             <div>
-                {products.map((item,index)=>{
-                    <ProductCart key={index} item={item}/>
-                })}
+                <div>
+                    <h2 className='text-4xl font-bold text-center my-8'>{heading}</h2>
+                    <div className='bg-slate-400 max-w-md mx-auto sm:rounded-full md:p-2.5 mb-16'>
+                        <div className='flex md:justify-evenly sm:flex-row justify-center'>
+                            {category.map((item) =>
+                                <button key={item} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-amber-800 hover:text-white transition-colors`}>{item}</button>
+                            )}
+                        </div>
+                    </div>
+                    <div className='grid m-20 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                        {products.map((item, index) => {
+                            return <ProductCart key={index} item={item} />
+                        })}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div> 
-    </>
-  )
+        </>
+    )
 }
 
 export default Products
